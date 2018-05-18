74393e6063c9f34786f1265ed24d0d613f698090
Juan Adolfo Bausa Pérez <ja.bausa@alumnos.upm.es>
2018-05-18 11:19:17 +0200

    P67

--- models/quiz.js
+++ models/quiz.js
@@ -3,7 +3,8 @@
 
 module.exports = function (sequelize, DataTypes) {
 
-    sequelize.define('quiz',
+
+    return sequelize.define('quiz',
         {
             question: {
                 type: DataTypes.STRING,
@@ -13,21 +14,5 @@ module.exports = function (sequelize, DataTypes) {
                 type: DataTypes.STRING,
                 validate: {notEmpty: {msg: "Answer must not be empty"}}
             }
-        });
-    sequelize.sync()
-    .then(() => sequelize.models.quiz.count())
-    .then(count => {
-        if(!count) {
-            return sequelize.models.quiz.bulkCreate([
-                { question: "Capital de Italia", answer : "Roma" },
-                { question: "Capital de Francia", answer : "París" },
-                { question: "Capital de España", answer : "Madrid" },
-                { question: "Capital de Portugal", answer : "Lisboa" }
-            ]);
-        }
-    })
-    .catch(error => {
-        console.log(error);
-    });
-    return sequelize;
+        })
 };
